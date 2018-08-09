b5204d0aad2bea2922e66acd2baa1f413d836906
sagarW <sagar10018233@gmail.com>
2018-08-09 16:02:48 +0530

    Description added to json

--- client/modules/main/js/mainController.js
+++ client/modules/main/js/mainController.js
@@ -8,6 +8,7 @@ angular.module('ctrl.dashboard', [])
             "ids": "p1",
             "productimage": 'images/product_one.jpg',
             "productname": "Maggi 70g",
+            "description":"Maggi is a product that belongs to Nestle India, which is one of the leading fast food companies in the country. The various Maggi products are stocks, instant noodles, ketchups and instant soups.",
             "price": 567,
             "ratings": 3
         },
@@ -15,6 +16,8 @@ angular.module('ctrl.dashboard', [])
                 "ids": "p2",
                 "productimage": 'images/product_two.jpg',
                 "productname": "24 Mantra Organic Sonamasuri Brown Raw Rice, 5kg",
+                "description":"This rice is finding increasing acceptance in diet menus prepared by master chefs across the world\n" +
+                    "It can be used to make Idli, dosa and other rice preparations",
                 "price": 240,
                 "ratings": 5
 
@@ -23,6 +26,7 @@ angular.module('ctrl.dashboard', [])
                 "ids": "p3",
                 "productimage": 'images/product_three.jpg',
                 "productname": "24 Mantra Organic Sugar, 1kg",
+                "decription":"24 mantra brings to you organic sugar that is perfect for every health-conscious person who cares for natural products.",
                 "price": 456,
                 "ratings": 5
             },
@@ -44,6 +48,7 @@ angular.module('ctrl.dashboard', [])
                 "ids": "p6",
                 "productimage": 'images/product_three.jpg',
                 "productname": "Saffola Salt Plus - Less Sodium, 1kg Pack",
+                "description":"Saffola Less Sodium Salt is also recognized as Saffola salt plus as an improved substitute to regular table salt.",
                 "price": 400,
                 "ratings": 3
             },
@@ -51,6 +56,7 @@ angular.module('ctrl.dashboard', [])
                 "ids": "p7",
                 "productimage": 'images/product_one.jpg',
                 "productname": "Agro Fresh Popcorn Maize, 200g",
+                "description":"Agro Fresh Popcorn seeds are packed with minerals, vitamins, protein and essential calories. If you are on a diet chart, do not forget to include this healthy snack",
                 "price": 23,
                 "ratings": 5
             },
@@ -58,6 +64,7 @@ angular.module('ctrl.dashboard', [])
                 "ids": "p8",
                 "productimage": 'images/product_two.jpg',
                 "productname": "Happilo Premium International Omani Dates, 250g (Pack of 1)",
+                "description":"100 Percent naturally dried and free from any additives or preservatives; These dates are high in potassium and helps prevent diseases like diarrhoea",
                 "price": 350,
                 "ratings": 5
             },
@@ -65,6 +72,7 @@ angular.module('ctrl.dashboard', [])
                 "ids": "p9",
                 "productimage": 'images/product_three.jpg',
                 "productname": "Patanjali Cow's Ghee, 1L",
+                "description":"Ghee calms Pitta and Vata. Hence, it is ideal for people with Vata-Pitta body type and for those suffering from Vata and Pitta imbalance disorders",
                 "price": 200,
                 "ratings": 5
             },
@@ -72,6 +80,7 @@ angular.module('ctrl.dashboard', [])
                 "ids": "p10",
                 "productimage": 'images/product_one.jpg',
                 "productname": "Patanjali Doodh Biscuits, 100g",
+                "description":"Take a well-deserved break with wholesome Patanjali Doodh Biscuits; Enhanced with fibre, the 100% atta biscuits are enriched with cow's milk",
                 "price": 100,
                 "ratings": 5
             }]
